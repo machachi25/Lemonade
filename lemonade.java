79c531a49006cacef669d96866bc032ea68cb3bd
Chinmay <machachi25@7hills.org>
2023-09-18 12:41:24 -0400

    Started the descriptions of my store

---
+++ lemonade.java
@@ -0,0 +1,9 @@
+public class Lemonade
+{
+    public static void main(String[] args)
+    {
+        double startingMoney = 20.00;
+        System.out.println("CJ's Lemonade Store");
+        System.out.println("Ice: $0.50 per cup")
+    }
+}

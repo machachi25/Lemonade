f5328014491f4dd3d31b0f30204d1452c3062c2d
Chinmay <machachi25@7hills.org>
2023-09-18 12:59:44 -0400

    Creating my variables for the shop

--- lemonade.java
+++ lemonade.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
+
 public class Lemonade
 {
     public static void main(String[] args)
     {
         double startingMoney = 20.00;
+        String enter;
+        String pitcher = "Pitcher";
+        dobule priceGlass = 
+        Scanner scan = new Scanner(System.in);
         System.out.println("CJ's Lemonade Store");
-        System.out.println("Ice: $0.50 per cup")
+        System.out.println("Ice: $0.50 per cup");
+        System.out.println("Cups: $0.10 per cup");
+        System.out.println("Lemons: $1.00 per cup");
+        System.out.println("Sugar: $0.54 per cup");
+        
+
     }
 }

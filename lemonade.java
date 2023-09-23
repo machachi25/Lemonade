979f85bda9b4d1740495a5315dd32196066107b2
Chinmay Machavaram <machachi25@7hills.org>
2023-09-23 12:18:39 -0400

    Adding a random generator for boolean to decide if buyed or not

--- lemonade.java
+++ lemonade.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.random;
 
 
 public class Lemonade
@@ -9,6 +10,8 @@ public class Lemonade
         String enter;
         String pitcher = "Pitcher";
         Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        int rn = random.nextInt(100);
         System.out.println("CJ's Lemonade Store");
         System.out.println("Ice: $0.50 per cup");
         System.out.println("Cups: $0.10 per cup");
